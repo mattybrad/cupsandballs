a731e00b9e95e25ce4334d73a215c6b2a681ae3f
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2016-12-24 18:20:39 +0000

    an ever-growing component

--- src/components/CodeBackground/index.jsx
+++ src/components/CodeBackground/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
 export default class CodeBackground extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      testList: []
+    }
+    setInterval(this.onInterval.bind(this), 100);
+  }
+
+  onInterval() {
+    var currentTestList = this.state.testList.slice();
+    currentTestList.push("hello");
+    this.setState({
+      testList: currentTestList
+    })
+  }
+
   render() {
     return(
-      <h1>code bg component</h1>
+      <div>
+        {this.state.testList.map(function(item){
+          return <div>{item}</div>
+        })}
+      </div>
     )
   }
 }
