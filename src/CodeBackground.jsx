05a50e5f71742b9eeb47ff15ad7b2e6c938e5315
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2016-12-25 02:42:48 +0000

    stripped out the annoying diff stuff for now

--- src/CodeBackground.jsx
+++ src/CodeBackground.jsx
@@ -1,43 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as JsDiff from 'diff';
+//import * as JsDiff from 'diff';
 import pretty from 'pretty';
 
 class CodeBackground extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      transitionalHTML: ""
+      prettyHTML: ""
     }
-    window.requestAnimationFrame(this.onInterval.bind(this));
+    setInterval(this.onInterval.bind(this), 100);
   }
 
   onInterval() {
     var thisHTML = pretty(document.getElementById('app').innerHTML);
-    var diff = JsDiff.diffChars(this.state.transitionalHTML, thisHTML);
-    //var diff = [];
-    var output = "";
-    var changeDone = false;
-    diff.map(function(d){
-      if(!d.added&&!d.removed) {
-        output += d.value;
-      } else if(d.added) {
-        if(!changeDone) output += d.value.slice(0, 1);
-        changeDone = true;
-      } else if(d.removed) {
-        output += changeDone ? d.value : d.value.slice(1);
-        changeDone = true;
-      }
-    })
     this.setState({
-      transitionalHTML: output
+      prettyHTML: thisHTML
     })
-    window.requestAnimationFrame(this.onInterval.bind(this));
   }
 
   render() {
     return(
-      <pre>{this.state.transitionalHTML}</pre>
+      <pre>{this.state.prettyHTML}</pre>
     )
   }
 }
