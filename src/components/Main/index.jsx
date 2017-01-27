545c2bd1358177557f845de20c86446cfcab0cfb
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-01-27 18:48:25 +0000

    getting the style back

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import classNames from 'classnames';
 import CupsAndBalls from '../CupsAndBalls';
+import styles from './index.css';
 
 export default class Main extends React.Component {
 
   render() {
     return(
-      <div>
-        {this.props.children}
+      <div className={classNames(styles.this)}>
+        <div className={classNames(styles.content)}>
+          {this.props.children}
+        </div>
         <CupsAndBalls mainAppComponent={this.props.children} />
       </div>
     )
