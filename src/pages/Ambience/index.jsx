ad10bc1c01227a47411d05aac313e98cdac02719
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-01-28 18:22:15 +0000

    ambience text

--- src/pages/Ambience/index.jsx
+++ src/pages/Ambience/index.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import classNames from 'classnames';
 import BackgroundDefinition from '../../components/BackgroundDefinition';
 
-
 export default class About extends React.Component {
 
   render() {
     return(
       <div>
         <BackgroundDefinition primaryColor='#ccff00' secondaryColor='#cc0000' />
-
         <h1>Ambience</h1>
-        <p>Here is some text.</p>
+          <p>I wanted this website to be fun, nerdy and interactive, rather than slick and minimalistic. To this end, there are a few optional features (animated background, algorithmic muzak, etc) that you won't find on most sites. If you would like to disable any of these features for a more relaxing browsing experience, you can do so below.</p>
+          <p>Click here to disable all extra features</p>
+          <p>Click here to disable the background music</p>
+          <p>Click here to disable the animation background</p>
+          <p>Click here to disable the console log</p>
+          <p>Click here to disable the DOM preview</p>
       </div>
     )
   }
