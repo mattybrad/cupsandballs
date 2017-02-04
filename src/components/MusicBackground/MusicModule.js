f0f99aa797ced885d4eb6f13ae9b721ecab6d27c
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-02-04 14:58:00 +0000

    gain node for each module

--- src/components/MusicBackground/MusicModule.js
+++ src/components/MusicBackground/MusicModule.js
@@ -3,6 +3,9 @@ export default class MusicModule {
     this.actx = actx;
     this.def = def;
     this.alive = true;
+    this.mainNode = this.actx.createGain();
+    this.mainNode.gain.value = 0.1;
+    this.mainNode.connect(this.actx.destination);
     setTimeout(this.tick.bind(this), 1000);
   }
 
@@ -10,7 +13,7 @@ export default class MusicModule {
     if(this.def.freq) {
       var osc = this.actx.createOscillator();
       osc.frequency.value = this.def.freq;
-      osc.connect(this.actx.destination);
+      osc.connect(this.mainNode);
       osc.start();
       osc.stop(this.actx.currentTime + 0.1);
     }
