5429b54b80284e276c73c83ade9ca181175f09a9
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-02-12 13:12:07 +0000

    working without silly chunk loader thing

--- src/pages/Projects/Randomspiel/index.jsx
+++ src/pages/Projects/Randomspiel/index.jsx
@@ -4,15 +4,28 @@ import BackgroundDefinition from '../../../components/BackgroundDefinition';
 import MusicDefinition from '../../../components/MusicDefinition';
 import musicDef from './musicDef';
 
-import TestLoader from '../../../components/chunks/Test.js';
-
-
 export default class Project extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      Test: null
+    }
+  }
+
   componentDidMount() {
-    TestLoader().then(({ test }) => {
-      this.setState({ test });
-    });
+    require.ensure([], () => {
+      var Test = require('../../../components/Test');
+      setTimeout(function() {
+        this.setState({
+          Test: Test
+        })
+      }.bind(this), 2000);
+    })
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.Test.default);
   }
 
   render() {
@@ -24,6 +37,7 @@ export default class Project extends React.Component {
         <p>This project was originally meant to be a big, real-life musical instrument. The instrument would consist of a hopper full of marbles cascading down a vertical board full of nails, with the marbles hitting random glockenspiel tines at the bottom.</p>
         <p>The idea was to replicate the {"random arpeggio"} function of a synthesizer without the use of a computer, partly because I was interested in the way that computers are oddly bad at generating random numbers, but mainly because it seemed like a fun thing to do.</p>
         <p>The project proved too ambitious at the time, but I did salvage something from it in the form of an interactive simulation, which can be played with below.</p>
+        {this.state.Test?<this.state.Test.default />:null}
       </div>
     )
   }
