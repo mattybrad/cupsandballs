26d25aeb4ba4dbc34892a72bfe50784fdcd23b03
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-02-13 21:47:32 +0000

    looking nice!

--- src/components/Randomspiel/index.jsx
+++ src/components/Randomspiel/index.jsx
@@ -9,6 +9,7 @@ export default class Randomspiel extends React.Component {
   componentDidMount() {
     var game = new Phaser.Game(760, 600, Phaser.AUTO, this.refs.phaserDiv);
     var game_state = {};
+    var ball;
 
     game_state.main = function() { };
     game_state.main.prototype = {
@@ -21,33 +22,55 @@ export default class Randomspiel extends React.Component {
         game.physics.startSystem(Phaser.Physics.P2JS);
         game.physics.p2.gravity.y = 2000;
         game.physics.p2.setImpactEvents(true);
-        game.physics.p2.restitution = 1;
+        game.physics.p2.restitution = 0.8;
 
         var ballCollisionGroup = game.physics.p2.createCollisionGroup();
         var pinCollisionGroup = game.physics.p2.createCollisionGroup();
 
-        game.physics.p2.updateBoundsCollisionGroup();
+        //game.physics.p2.updateBoundsCollisionGroup();
 
-        this.ball = game.add.sprite(300, -50, 'ball');
-        game.physics.p2.enable(this.ball);
-        this.ball.body.fixedRotation = true;
-        this.ball.body.setCollisionGroup(ballCollisionGroup);
-        this.ball.body.collides(pinCollisionGroup);
+        ball = game.add.sprite(game.width / 2, 10, 'ball');
+        game.physics.p2.enable(ball);
+        this.resetBall();
+        ball.body.setCircle(25);
+        ball.body.setCollisionGroup(ballCollisionGroup);
+        ball.body.collides(pinCollisionGroup);
 
         var pins = game.add.group();
         pins.enableBody = true;
         pins.physicsBodyType = Phaser.Physics.P2JS;
-        for(var i = 0; i < 30; i ++) {
-          var pin = pins.create(Math.random() * 800, Math.random() * 500, 'ball');
-          pin.body.setRectangle(50,50);
-          pin.body.static = true;
-          pin.body.setCollisionGroup(pinCollisionGroup);
-          pin.body.collides(ballCollisionGroup);
+        var pinGroupHeight = 0.7; // fraction of overall height
+        var pinGroupTopSpace = 0.15; // fraction of overall height
+        var cols = 10;
+        var rows = 8;
+        var oddRow;
+        for(var i = 0; i < rows; i ++) {
+          oddRow = !!(i%2);
+          for(var j = 0; j < (oddRow ? cols : cols - 1); j ++) {
+            var pin = pins.create(
+              (oddRow ? j+1 : j+1.5) * game.width / (cols + 1),
+              pinGroupTopSpace * game.height + i * pinGroupHeight * game.height / (rows - 1),
+              'ball'
+            );
+            pin.scale.set(0.2);
+            pin.body.setCircle(5);
+            pin.body.static = true;
+            pin.body.setCollisionGroup(pinCollisionGroup);
+            pin.body.collides(ballCollisionGroup);
+          }
         }
       },
 
-      update: function() {
+      resetBall() {
+        ball.body.y = 10;
+        ball.body.x = game.width / 2 + 5 * (1-Math.random());
+        ball.body.velocity.x = ball.body.velocity.y = 0;
+      },
 
+      update: function() {
+        if(ball.body.y > 600) {
+          this.resetBall();
+        }
       }
     };
 
