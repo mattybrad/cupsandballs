8cfbe60f0c8377a03eccf315e0a0d05c7b214309
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-02-13 19:31:29 +0000

    nice working physics test

--- src/components/Randomspiel/index.jsx
+++ src/components/Randomspiel/index.jsx
@@ -4,33 +4,53 @@ window.PIXI = require('phaser/build/custom/pixi');
 window.p2 = require('phaser/build/custom/p2');
 window.Phaser = require('phaser/build/custom/phaser-split');
 
-export default class Test extends React.Component {
+export default class Randomspiel extends React.Component {
 
   componentDidMount() {
     var game = new Phaser.Game(760, 600, Phaser.AUTO, this.refs.phaserDiv);
     var game_state = {};
 
-    // Creates a new 'main' state that wil contain the game
     game_state.main = function() { };
     game_state.main.prototype = {
 
       preload: function() {
-  		// Function called first to load all the assets
-          game.load.image('hello', '/assets/images/pluto.png');
+        game.load.image('ball', '/assets/images/ball.png');
       },
 
-      create: function() {
-      	// Fuction called after 'preload' to setup the game
-          this.hello_sprite = game.add.sprite(250, 300, 'hello');
+      create: function()  {
+        game.physics.startSystem(Phaser.Physics.P2JS);
+        game.physics.p2.gravity.y = 2000;
+        game.physics.p2.setImpactEvents(true);
+        game.physics.p2.restitution = 1;
+
+        var ballCollisionGroup = game.physics.p2.createCollisionGroup();
+        var pinCollisionGroup = game.physics.p2.createCollisionGroup();
+
+        game.physics.p2.updateBoundsCollisionGroup();
+
+        this.ball = game.add.sprite(300, -50, 'ball');
+        game.physics.p2.enable(this.ball);
+        this.ball.body.fixedRotation = true;
+        this.ball.body.setCollisionGroup(ballCollisionGroup);
+        this.ball.body.collides(pinCollisionGroup);
+
+        var pins = game.add.group();
+        pins.enableBody = true;
+        pins.physicsBodyType = Phaser.Physics.P2JS;
+        for(var i = 0; i < 30; i ++) {
+          var pin = pins.create(Math.random() * 800, Math.random() * 500, 'ball');
+          pin.body.setRectangle(50,50);
+          pin.body.static = true;
+          pin.body.setCollisionGroup(pinCollisionGroup);
+          pin.body.collides(ballCollisionGroup);
+        }
       },
 
       update: function() {
-  		// Function called 60 times per second
-          this.hello_sprite.angle += 1;
+
       }
     };
 
-    // Add and start the 'main' state to start the game
     game.state.add('main', game_state.main);
     game.state.start('main');
   }
