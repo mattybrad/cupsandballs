6ef1a856c0e9f78281dd2ef7ca1a370cac0b49a3
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-02-21 22:39:33 +0000

    yeah that looks nice

--- src/pages/About/SelfPortrait/index.jsx
+++ src/pages/About/SelfPortrait/index.jsx
@@ -10,6 +10,7 @@ export default class SelfPortait extends React.Component {
     img.addEventListener("load", this.onImageLoaded.bind(this, img));
     img.src = "/assets/images/portrait.png";
     this.doFrame();
+    this.startTime = Date.now();
   }
 
   onImageLoaded(img) {
@@ -23,6 +24,7 @@ export default class SelfPortait extends React.Component {
 
   doFrame() {
     var c = this.ctx;
+    var elapsed = Date.now() - this.startTime;
     if(this.imgCtx) {
       for(var i = 0; i < 10; i ++) {
         var x = Math.floor(Math.random() * c.canvas.width);
@@ -32,11 +34,13 @@ export default class SelfPortait extends React.Component {
         var trans = imgData[3] / 255;
         var faceDistance = Math.sqrt(Math.pow(x-90,2)+Math.pow(y-200,2));
         c.beginPath();
-        c.arc(x,y,Math.max(2, faceDistance / 20),0,2*Math.PI);
+        c.arc(x,y,Math.max(2, 30 * (1 - elapsed / 8000), faceDistance / 20),0,2*Math.PI);
         if(trans > 0.2) {
+          c.globalAlpha = 0.6;
           c.fillStyle = "rgb("+[whiteness,whiteness,whiteness].join(",")+")";
         } else {
-          c.fillStyle = Math.random() > 0.5 ? "#003300" : "#339933";
+          c.globalAlpha = 0.3;
+          c.fillStyle = Math.random() > 0.5 ? "#cccccc" : "#999999";
         }
         c.fill();
       }
